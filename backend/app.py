f25c503f8d5f270d8b97a3cee1724f36ea3d2698
lalithya-k <123735779+lalithya-k@users.noreply.github.com>
2024-10-06 12:16:49 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,10 @@ from database.db import initialize_db
 app = Flask(__name__)
 CORS(app)
 
+CORS(app, resources={r"/*":{"origins": "http://localhost:5173",
+"methods":["GET","POST","DELETE","OPTIONS"]
+}})
+
 # Configuration for MongoDB
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/budget_app'  # Update with your MongoDB URI
 initialize_db(app)
